39f07c6b2e8c92b4c0bb3d05dbe0c025083af99a
adnatanael <70278519+adnatanael@users.noreply.github.com>
2025-02-11 12:47:46 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,18 @@
-import { Analytics } from "@vercel/analytics/react";
-import { AppPropsWithLayout } from "../types";
-import { Hydrate, QueryClientProvider } from "@tanstack/react-query";
-import { RootLayout } from "src/layouts";
-import { queryClient } from "src/libs/react-query";
+import { AppPropsWithLayout } from "../types"
+import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
+import { RootLayout } from "src/layouts"
+import { queryClient } from "src/libs/react-query"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout || ((page) => page);
+  const getLayout = Component.getLayout || ((page) => page)
 
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <RootLayout>
-          {getLayout(<Component {...pageProps} />)}
-          <Analytics /> {/* Add Analytics here */}
-        </RootLayout>
+        <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
       </Hydrate>
     </QueryClientProvider>
-  );
+  )
 }
 
-export default App;
+export default App
